fd08098baff01979e9fd2431afac8e1df4e7e70f
Connor Talbot <connordanielfrank@gmail.com>
2024-01-24 17:22:25 +1300

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -18,177 +18,207 @@ export default function Home() {
   }, [cardRef]);
 
   return (
-    <main className=" flex min-h-screen flex-col gap-0">
-      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
-        <BackgroundText
-          repeats={numRepeats}
-          text="MOBILE DESKTOP TABLET VIEW RESPONSIVE DESIGN EXTENSION CHROME EDGE FIREFOX SAFARI"
-          opacity="opacity-20"
-          color="text-[#FF10F0]"
-        />
-        <div
-          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
-          ref={cardRef}
-        >
+    <main className="min-h-screen  p-[40px]">
+      <section className="w-full h-full flex flex-wrap bg-white">
+        <div className="relative flex flex-col w-full items-center p-20 justify-center overflow-clip whitespace-nowrap">
+          <BackgroundText
+            repeats={numRepeats}
+            text="MOBILE DESKTOP TABLET VIEW RESPONSIVE DESIGN EXTENSION CHROME EDGE FIREFOX SAFARI"
+            opacity="opacity-20"
+            color="text-[#FF10F0]"
+          />
           <div
-            onMouseEnter={() => setIsHoveredMobileView(true)}
-            onMouseLeave={() => setIsHoveredMobileView(false)}
-            className="relative cursor-help"
+            className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+            ref={cardRef}
           >
-            <h2 className="text-5xl font-bold font-serif leading-10">
-              MOBILE VIEW
-            </h2>
-            <img
-              src="mobile_view.png"
-              width={375}
-              height={236}
-              alt="Mobile View Extension"
-              className="border-2 border-black"
-            />
-            {isHoveredMobileView && (
-              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
-                <p className="text-wrap whitespace-pre-wrap text-sans">
-                  See your website on multiple devices at once with live
-                  updates, even for locally hosted sites. Hot module reloading
-                  ensures instant code changes across all devices, while
-                  synchronised scrolling and navigation lets you test seamlessly
-                  across any website.
-                </p>
-              </div>
-            )}
-          </div>
-          <div className="flex items-center justify-center gap-4 p-2">
-            <a
-              href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
-              target="_blank"
-              className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-pointer leading-none font-bold text-lg"
+            <div
+              onMouseEnter={() => setIsHoveredMobileView(true)}
+              onMouseLeave={() => setIsHoveredMobileView(false)}
+              className="relative cursor-help"
             >
-              CHROME
-            </a>
-            <a
-              href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
-              target="_blank"
-              className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-pointer leading-none font-bold text-lg"
-            >
-              EDGE
-            </a>
+              <h2 className="text-5xl font-bold font-serif leading-10">
+                MOBILE VIEW
+              </h2>
+              <img
+                src="mobile_view.png"
+                width={375}
+                height={236}
+                alt="Mobile View Extension"
+                className="border-2 border-black"
+              />
+              {isHoveredMobileView && (
+                <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                  <p className="text-wrap whitespace-pre-wrap text-sans">
+                    See your website on multiple devices at once with live
+                    updates, even for locally hosted sites. Hot module reloading
+                    ensures instant code changes across all devices, while
+                    synchronised scrolling and navigation lets you test
+                    seamlessly across any website.
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-center gap-4 p-2">
+              <a
+                href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
+                target="_blank"
+                className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-pointer leading-none font-bold text-lg"
+              >
+                CHROME
+              </a>
+              <a
+                href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
+                target="_blank"
+                className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-pointer leading-none font-bold text-lg"
+              >
+                EDGE
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
-        <BackgroundText
-          repeats={numRepeats}
-          text="SEEK SALARY MONEY JOBS WORKING EXTENSION CHROME EDGE FIREFOX SAFARI"
-          color="text-[#FF5F1F]"
-          opacity="opacity-20"
-        />
-        <div
-          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
-          ref={cardRef}
-        >
+        <div className="relative flex flex-col w-full items-center p-20 justify-center  overflow-clip whitespace-nowrap">
+          <BackgroundText
+            repeats={numRepeats}
+            text="SEEK SALARY MONEY JOBS WORKING EXTENSION CHROME EDGE FIREFOX SAFARI"
+            color="text-[#FF5F1F]"
+            opacity="opacity-20"
+          />
           <div
-            onMouseEnter={() => setIsHoveredSeek(true)}
-            onMouseLeave={() => setIsHoveredSeek(false)}
-            className="relative cursor-help"
+            className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+            ref={cardRef}
           >
-            <h2 className="text-5xl font-bold font-serif leading-10">
-              SEEK SALARY
-            </h2>
-            <img
-              src="seek_salary_peek.png"
-              width={375}
-              height={236}
-              alt="Seek Salary Extension"
-              className="border-2 border-black"
-            />
-            {isHoveredSeek && (
-              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
-                <p className="text-wrap whitespace-pre-wrap text-sans">
-                  Get real salary ranges for every job on Seek, giving you a
-                  competitive edge in negotiations. Make informed career
-                  decisions, use salary data as a baseline for salary
-                  negotiations, ensuring you get the compensation you deserve.
-                </p>
-              </div>
-            )}
-          </div>
-          <div className="flex items-center justify-center gap-4 p-2 text-lg">
-            <a
-              target="_blank"
-              href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
-              className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-pointer leading-none font-bold"
+            <div
+              onMouseEnter={() => setIsHoveredSeek(true)}
+              onMouseLeave={() => setIsHoveredSeek(false)}
+              className="relative cursor-help"
             >
-              CHROME
-            </a>
-            <a
-              target="_blank"
-              href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
-              className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-pointer leading-none font-bold"
-            >
-              EDGE
-            </a>
-            <a
-              target="_blank"
-              href="https://addons.mozilla.org/en-US/firefox/addon/seek-salary/"
-              className="border-2 text-[#d65126] border-[#d65126] shadow-[3px_3px_0_0_#d65126] hover:scale-95 cursor-pointer leading-none font-bold"
-            >
-              FIREFOX
-            </a>
+              <h2 className="text-5xl font-bold font-serif leading-10">
+                SEEK SALARY
+              </h2>
+              <img
+                src="seek_salary_peek.png"
+                width={375}
+                height={236}
+                alt="Seek Salary Extension"
+                className="border-2 border-black"
+              />
+              {isHoveredSeek && (
+                <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                  <p className="text-wrap whitespace-pre-wrap text-sans">
+                    Get real salary ranges for every job on Seek, giving you a
+                    competitive edge in negotiations. Make informed career
+                    decisions, use salary data as a baseline for salary
+                    negotiations, ensuring you get the compensation you deserve.
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-center gap-4 p-2 text-lg">
+              <a
+                target="_blank"
+                href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
+                className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-pointer leading-none font-bold"
+              >
+                CHROME
+              </a>
+              <a
+                target="_blank"
+                href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
+                className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-pointer leading-none font-bold"
+              >
+                EDGE
+              </a>
+              <a
+                target="_blank"
+                href="https://addons.mozilla.org/en-US/firefox/addon/seek-salary/"
+                className="border-2 text-[#d65126] border-[#d65126] shadow-[3px_3px_0_0_#d65126] hover:scale-95 cursor-pointer leading-none font-bold"
+              >
+                FIREFOX
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
-        <BackgroundText
-          repeats={numRepeats}
-          text="UI/UX COMING SOON DESIGN INSPIRATION AI AUTOMATION EXTENSION CHROME EDGE FIREFOX SAFARI"
-          color="text-[#4C8CF4]"
-          opacity="opacity-20"
-        />
-        <div
-          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
-          ref={cardRef}
-        >
+        <div className="relative flex flex-col w-full items-center p-20 justify-center overflow-clip whitespace-nowrap">
+          <BackgroundText
+            repeats={numRepeats}
+            text="UI/UX COMING SOON DESIGN INSPIRATION AI AUTOMATION EXTENSION CHROME EDGE FIREFOX SAFARI"
+            color="text-[#4C8CF4]"
+            opacity="opacity-20"
+          />
           <div
-            onMouseEnter={() => setIsHoveredUICopilot(true)}
-            onMouseLeave={() => setIsHoveredUICopilot(false)}
-            className="relative cursor-help"
+            className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+            ref={cardRef}
           >
-            <h2 className="text-5xl font-bold font-serif leading-10">
-              UI COPILOT
-            </h2>
-            <img
-              src="shhh.png"
-              width={375}
-              height={236}
-              alt="Mobile View Extension"
-              className="border-2 border-black max-h-[236px]"
-            />
-            {isHoveredUICopilot && (
-              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
-                <p className="text-wrap whitespace-pre-wrap text-sans">
-                  This extension will automatically give you component level and
-                  project level design feedback while you code! You set the
-                  style, and work as normal
-                </p>
-              </div>
-            )}
-          </div>
-          <div className="flex items-center justify-center gap-4 p-2 text-lg">
-            <button className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-not-allowed leading-none font-bold">
-              CHROME
-            </button>
-            <button className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-not-allowed leading-none font-bold">
-              EDGE
-            </button>
-            <button className="border-2 text-[#d65126] border-[#d65126] shadow-[3px_3px_0_0_#d65126] hover:scale-95 cursor-not-allowed leading-none font-bold">
-              FIREFOX
-            </button>
-            <button className="border-2 text-[#00d3f9] border-[#00d3f9] shadow-[3px_3px_0_0_#00d3f9] hover:scale-95 cursor-not-allowed leading-none font-bold">
-              SAFARI
-            </button>
+            <div
+              onMouseEnter={() => setIsHoveredUICopilot(true)}
+              onMouseLeave={() => setIsHoveredUICopilot(false)}
+              className="relative cursor-help"
+            >
+              <h2 className="text-5xl font-bold font-serif leading-10">
+                UI COPILOT
+              </h2>
+              <img
+                src="shhh.png"
+                width={375}
+                height={236}
+                alt="Mobile View Extension"
+                className="border-2 border-black max-h-[236px]"
+              />
+              {isHoveredUICopilot && (
+                <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                  <p className="text-wrap whitespace-pre-wrap text-sans">
+                    This extension will automatically give you component level
+                    and project level design feedback while you code! You set
+                    the style, and work as normal
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="flex items-center justify-center gap-4 p-2 text-lg">
+              <button className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[3px_3px_0_0_#4C8CF4] hover:scale-95 cursor-not-allowed leading-none font-bold">
+                CHROME
+              </button>
+              <button className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[3px_3px_0_0_#2bc3d2] hover:scale-95 cursor-not-allowed leading-none font-bold">
+                EDGE
+              </button>
+              <button className="border-2 text-[#d65126] border-[#d65126] shadow-[3px_3px_0_0_#d65126] hover:scale-95 cursor-not-allowed leading-none font-bold">
+                FIREFOX
+              </button>
+              <button className="border-2 text-[#00d3f9] border-[#00d3f9] shadow-[3px_3px_0_0_#00d3f9] hover:scale-95 cursor-not-allowed leading-none font-bold">
+                SAFARI
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+        {/* FRAME */}
+        {/* TOP */}
+        <div className="fixed top-0 left-[40px] right-[40px] bg-pink-500 text-white z-20">
+          <p className="p-2">
+            Only you have the power to click the links below
+          </p>
+        </div>
+
+        {/* RIGHT */}
+        <div className="fixed top-0 bottom-0 right-0 flex items-center justify-center  bg-pink-500 text-white max-w-[40px] z-20">
+          <p className="transform rotate-90 whitespace-nowrap">
+            Only you have the power to click the links below
+          </p>
+        </div>
+
+        {/* LEFT */}
+        <div className="fixed top-0 bottom-0 left-0 flex items-center justify-center  bg-pink-500 text-white max-w-[40px] z-20">
+          <p className="transform rotate-90 whitespace-nowrap">
+            Only you have the power to click the links below
+          </p>
+        </div>
+
+        {/* BOTTOM */}
+        <div className="fixed bottom-0 left-[40px] right-[40px] flex items-center justify-center  bg-pink-500 text-white z-20">
+          <p className="p-2 whitespace-nowrap">
+            Only you have the power to click the links below
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
