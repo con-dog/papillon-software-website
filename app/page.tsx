9af42def8b0f54f4e5c07ce07b6432acff43c0c0
Connor Talbot <connordanielfrank@gmail.com>
2024-01-24 14:35:35 +1300

    fin

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import { BackgroundText } from "@/components/background-text";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 export default function Home() {
-  const cardRef = useRef(null);
+  const cardRef = useRef<HTMLDivElement>(null);
   const [numRepeats, setNumRepeats] = useState(0);
+  const [isHoveredUICopilot, setIsHoveredUICopilot] = useState(false);
+  const [isHoveredMobileView, setIsHoveredMobileView] = useState(false);
+  const [isHoveredSeek, setIsHoveredSeek] = useState(false);
 
   useEffect(() => {
     if (cardRef.current) {
@@ -12,42 +15,177 @@ export default function Home() {
       const textHeight = 96; // Approximate height of one line of text; adjust as needed
       const repeats = Math.ceil(cardHeight + 96 / textHeight);
       setNumRepeats(repeats);
-      console.log(cardHeight, textHeight, repeats);
     }
   }, [cardRef]);
 
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-between">
-      <div className="relative flex flex-col w-full items-center justify-center p-24 overflow-clip whitespace-nowrap">
+    <main className=" flex min-h-screen flex-col gap-0">
+      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
         <BackgroundText
           repeats={numRepeats}
-          text="MOBILE DESKTOP TABLET VIEW RESPONSIVE DESIGN EXTENSION CHROME EDGE FIREFOX"
+          text="MOBILE DESKTOP TABLET VIEW RESPONSIVE DESIGN EXTENSION CHROME EDGE FIREFOX SAFARI"
+          opacity="opacity-20"
+          color="text-[#FF10F0]"
+        />
+        <div
+          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+          ref={cardRef}
+        >
+          <div
+            onMouseEnter={() => setIsHoveredMobileView(true)}
+            onMouseLeave={() => setIsHoveredMobileView(false)}
+            className="relative cursor-help"
+          >
+            <h2 className="text-5xl font-bold font-serif leading-10">
+              MOBILE VIEW
+            </h2>
+            <Image
+              src="mobile_view.png"
+              width={375}
+              height={236}
+              alt="Mobile View Extension"
+              className="border-2 border-black"
+            />
+            {isHoveredMobileView && (
+              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                <p className="text-wrap text-sans">
+                  See your website on multiple devices at once with live
+                  updates, even for locally hosted sites. Hot module reloading
+                  ensures instant code changes across all devices, while
+                  synchronised scrolling and navigation lets you test seamlessly
+                  across any website.
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex items-center justify-center gap-4 p-2">
+            <a
+              href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
+              target="_blank"
+              className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[5px_5px_0_0_#4C8CF4] hover:scale-95 cursor-pointer"
+            >
+              Chrome
+            </a>
+            <a
+              href="https://chromewebstore.google.com/detail/mobile-view-test-responsi/clepmakjkiihmfoepipckkafafdepjne"
+              target="_blank"
+              className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[5px_5px_0_0_#2bc3d2] hover:scale-95 cursor-pointer"
+            >
+              Edge
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
+        <BackgroundText
+          repeats={numRepeats}
+          text="SEEK SALARY MONEY JOBS WORKING EXTENSION CHROME EDGE FIREFOX SAFARI"
+          color="text-[#FF5F1F]"
+          opacity="opacity-20"
+        />
+        <div
+          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+          ref={cardRef}
+        >
+          <div
+            onMouseEnter={() => setIsHoveredSeek(true)}
+            onMouseLeave={() => setIsHoveredSeek(false)}
+            className="relative cursor-help"
+          >
+            <h2 className="text-5xl font-bold font-serif leading-10">
+              SEEK SALARY
+            </h2>
+            <Image
+              src="seek_salary_peek.png"
+              width={375}
+              height={236}
+              alt="Seek Salary Extension"
+              className="border-2 border-black"
+            />
+            {isHoveredSeek && (
+              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                <p className="text-wrap text-sans">
+                  Get real salary ranges for every job on Seek, giving you a
+                  competitive edge in negotiations. Make informed career
+                  decisions, use salary data as a baseline for salary
+                  negotiations, ensuring you get the compensation you deserve.
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="flex items-center justify-center gap-4 p-2">
+            <a
+              target="_blank"
+              href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
+              className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[5px_5px_0_0_#4C8CF4] hover:scale-95 cursor-pointer"
+            >
+              Chrome
+            </a>
+            <a
+              target="_blank"
+              href="https://chromewebstore.google.com/detail/seek-salary-peek/edmpglkdfjgoidigadgedmgcabnmghfl"
+              className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[5px_5px_0_0_#2bc3d2] hover:scale-95 cursor-pointer"
+            >
+              Edge
+            </a>
+            <a
+              target="_blank"
+              href="https://addons.mozilla.org/en-US/firefox/addon/seek-salary/"
+              className="border-2 text-[#d65126] border-[#d65126] shadow-[5px_5px_0_0_#d65126] hover:scale-95 cursor-pointer"
+            >
+              Firefox
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="relative flex flex-col w-full items-center justify-center p-16 overflow-clip whitespace-nowrap">
+        <BackgroundText
+          repeats={numRepeats}
+          text="UI/UX COMING SOON DESIGN INSPIRATION AI AUTOMATION EXTENSION CHROME EDGE FIREFOX SAFARI"
           color="text-[#4C8CF4]"
           opacity="opacity-20"
         />
         <div
-          className="z-10 border-2 border-black bg-white shadow-[15px_15px_0_0_#000000]"
+          className="z-10 border-4 border-black bg-white shadow-[15px_15px_0_0_#000000]"
           ref={cardRef}
         >
-          <h1 className="text-5xl font-bold font-serif leading-10">
-            Mobile View
-          </h1>
-          <Image
-            src="mobile_view.png"
-            width={375}
-            height={812}
-            alt="Mobile View Extension"
-            className="border-2 border-black"
-          />
+          <div
+            onMouseEnter={() => setIsHoveredUICopilot(true)}
+            onMouseLeave={() => setIsHoveredUICopilot(false)}
+            className="relative cursor-help"
+          >
+            <h2 className="text-5xl font-bold font-serif leading-10">
+              UI COPILOT
+            </h2>
+            <Image
+              src="shhh.png"
+              width={375}
+              height={236}
+              alt="Mobile View Extension"
+              className="border-2 border-black max-h-[236px]"
+            />
+            {isHoveredUICopilot && (
+              <div className="absolute inset-0 bg-black bg-opacity-95 flex justify-center items-center text-white p-4">
+                <p className="text-wrap text-sans">
+                  This extension will automatically give you component level and
+                  project level design feedback while you code! You set the
+                  style, and work as normal
+                </p>
+              </div>
+            )}
+          </div>
           <div className="flex items-center justify-center gap-4 p-2">
-            <button className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[5px_5px_0_0_#4C8CF4]">
+            <button className="border-2 text-[#4C8CF4] border-[#4C8CF4] shadow-[5px_5px_0_0_#4C8CF4] hover:scale-95 cursor-not-allowed">
               Chrome
             </button>
-            <button className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[5px_5px_0_0_#2bc3d2]">
+            <button className="border-2 text-[#2bc3d2] border-[#2bc3d2] shadow-[5px_5px_0_0_#2bc3d2] hover:scale-95 cursor-not-allowed">
               Edge
             </button>
-            <button className="border-2 text-[#d65126] border-[#d65126] shadow-[5px_5px_0_0_#d65126]">
+            <button className="border-2 text-[#d65126] border-[#d65126] shadow-[5px_5px_0_0_#d65126] hover:scale-95 cursor-not-allowed">
               Firefox
+            </button>
+            <button className="border-2 text-[#00d3f9] border-[#00d3f9] shadow-[5px_5px_0_0_#00d3f9] hover:scale-95 cursor-not-allowed">
+              Safari
             </button>
           </div>
         </div>
--- components/background-text.tsx
+++ components/background-text.tsx
@@ -1,5 +1,5 @@
 "use client";
-const shuffleWords = (text) => {
+const shuffleWords = (text: string) => {
   let words = text.split(" ");
   // Simple shuffle algorithm
   for (let i = words.length - 1; i > 0; i--) {
@@ -9,14 +9,24 @@ const shuffleWords = (text) => {
   return words.join(" ");
 };
 
-export const BackgroundText = ({ text, repeats, color, opacity }) => {
+export const BackgroundText = ({
+  text,
+  repeats,
+  color,
+  opacity,
+}: {
+  text: string;
+  repeats: number;
+  color: string;
+  opacity: string;
+}) => {
   return (
     <div className="absolute w-full top-0">
       {Array.from({ length: repeats }, (_, i) => (
         <p
           key={i}
           className={`${color} ${opacity} text-5xl font-bold select-none pointer-events-none`}
-          style={{ wordSpacing: "-.15em", lineHeight: "0.8" }}
+          style={{ wordSpacing: "-.15em", lineHeight: "0.95" }}
         >
           {text}
         </p>
